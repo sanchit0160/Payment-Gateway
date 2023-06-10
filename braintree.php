8e293d5dccd5a7dcbb3fcd0ca9afc6c98ed59b27
sanchit0160 <36697768+sanchit0160@users.noreply.github.com>
2023-06-10 22:44:10 +0530

    Update braintree.php

--- braintree.php
+++ braintree.php
@@ -1,1 +1,25 @@
+<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://payments.braintree-api.com/graphql');
+curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  'Accept: */*',
+  'Authorization: Bearer production_6mhyzqmw_k588b3w67tw7q2zs',
+  'Braintree-Version: 2018-05-10',
+  'Connection: keep-alive',
+  'Content-Type: application/json',
+  'Host: payments.braintree-api.com',
+  'Origin: https://assets.braintreegateway.com',
+  'Referer: https://assets.braintreegateway.com/',
+  'Sec-Fetch-Dest: empty',
+  'Sec-Fetch-Mode: cors',
+  'Sec-Fetch-Site: cross-site'));
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"8c2659f3-1d63-408c-a435-1fdccc20ef1d"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"'.$cardNumber.'","expirationMonth":".$expmm.'","expirationYear":"2025",".$expyyyy.'":"'.$cvv.'"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}');
 
+$result = curl_exec($ch);
+?>

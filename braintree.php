74ccd80d70138370ab9c8b71910f90718f56c4ae
sanchit0160 <36697768+sanchit0160@users.noreply.github.com>
2023-06-12 23:46:34 +0530

    Update braintree.php

--- braintree.php
+++ braintree.php
@@ -5,7 +5,7 @@ curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
   'Accept: */*',
-  'Authorization: Bearer production_6mhyzqmw_k588b3w67tw7q2zs',
+  'Authorization: Bearer <BEARER_VALUE>',
   'Braintree-Version: 2018-05-10',
   'Connection: keep-alive',
   'Content-Type: application/json',
@@ -19,7 +19,7 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_POSTFIELDS, '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"8c2659f3-1d63-408c-a435-1fdccc20ef1d"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"'.$cardNumber.'","expirationMonth":".$expmm.'","expirationYear":"2025",".$expyyyy.'":"'.$cvv.'"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}');
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"8c2659f3-1d63-408c-a435-1fdccc20ef1d"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"'.$cardNumber.'","expirationMonth":".$expmm.'","expirationYear":"'.$expyyyy.'","cvv":"'.$cvv.'"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}');
 
 $result = curl_exec($ch);
 ?>

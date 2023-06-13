9ff14a2efc3c534af93ad4a8b1a870bfc5f7fc64
sanchit0160 <36697768+sanchit0160@users.noreply.github.com>
2023-06-13 23:47:33 +0530

    Update stripe.js

--- stripe.js
+++ stripe.js
@@ -1,1 +1,31 @@
+const url = 'https://api.stripe.com/v1/tokens';
+const params = new URLSearchParams({
+  time_on_page: timeOnPage,
+  pasted_fields: 'number',
+  guid,
+  muid,
+  sid,
+  key: pkLive,
+  payment_user_agent: 'stripe.js/78ef418',
+  'card[number]': cardn,
+  'card[cvc]': cvv,
+  'card[exp_month]': expmm,
+  'card[exp_year]': expyy
+});
 
+try {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Origin': 'https://js.stripe.com',
+      'Referer': 'https://js.stripe.com/'
+    },
+    body: params
+  });
+
+  const data = await response.json();
+} 
+catch (error) {
+}
